17dbdaf15af53bed50d6c51d1f42184410063b01
Bank <blankinfear@gmail.com>
2025-02-27 03:30:37 +0700

    set vuetify

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import Toast from "vue-toastification";
 import router from "./components/router/Router";
 import "vue-toastification/dist/index.css";
 import naive from "naive-ui";
+import vuetify from "./plugins/vuetify";
 
 const app = createApp(App);
 app.use(router);
 app.use(Toast);
 app.use(naive);
+app.use(vuetify);
 
 app.mount("#app");
